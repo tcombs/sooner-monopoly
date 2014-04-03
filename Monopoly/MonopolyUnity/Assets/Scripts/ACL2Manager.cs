46817204360f3a144b1081f7d0c54628219e5a8c
Colton Hurt <colton.t.hurt-1@ou.edu>
2014-04-03 16:45:39 -0500

    Added in roll dice script and ACL2 Manager.

---
+++ Monopoly/MonopolyUnity/Assets/Scripts/ACL2Manager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+public class ACL2Manager : MonoBehaviour {
+
+	private static ACL2Manager _instance;
+	public static ACL2Manager instance{ get {return _instance;}}
+	
+	public enum ACL2Options
+	{
+		BuyProperty,
+		RollDice,
+		MovePlayer,
+		PayPlayer,
+		UpgradeProperty
+	}
+	
+	private readonly string BUY_PROPERTY_EXE = "C:\\Users\\Colton\\Documents\\sooner-monopoly\\Monopoly\\MonopolyUnity\\Assets\\ACL2Modules\\BuyProperty.exe"; 
+	private readonly string ROLL_DICE_EXE = "C:\\Users\\Colton\\Documents\\sooner-monopoly\\Monopoly\\MonopolyUnity\\Assets\\ACL2Modules\\Roll.exe";
+	private readonly string MOVE_PLAYER_EXE = "C:\\Users\\Colton\\Documents\\sooner-monopoly\\Monopoly\\MonopolyUnity\\Assets\\ACL2Modules\\MovePlayerTo.exe";
+	private readonly string PAY_PLAYER_EXE = "C:\\Users\\Colton\\Documents\\sooner-monopoly\\Monopoly\\MonopolyUnity\\Assets\\ACL2Modules\\PayToPlayer.exe";
+	private readonly string UPGRADE_PROPERTY_EXE = "C:\\Users\\Colton\\Documents\\sooner-monopoly\\Monopoly\\MonopolyUnity\\Assets\\ACL2Modules\\UpgradeProperty.exe";
+
+	protected void Awake()
+	{
+		_instance = this;
+	}
+
+	public IEnumerator RunACL2(ACL2Options choice, string[] arguments)
+	{
+		//Do file io to write args and then call exe
+
+		yield return 0;
+	}
+}
---
+++ Monopoly/MonopolyUnity/Assets/Scripts/RollDice.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RollDice : MonoBehaviour {
+
+
+
+	void OnClick()
+	{
+		Debug.Log("Worked");
+		Debug.Log("The current player's turn is: " +GameManager.instance.currentTurnPlayerID);
+	
+		int randDie1 = Random.Range(1,7);
+		int randDie2 = Random.Range(1,7);
+		int playerIDRolled = GameManager.instance.currentTurnPlayerID;
+	
+		string[] args = {playerIDRolled.ToString() ,randDie1.ToString(), randDie2.ToString()}; 
+
+		StartCoroutine(ACL2Manager.instance.RunACL2(ACL2Manager.ACL2Options.RollDice, args));
+
+		GameManager.instance.spaces[(randDie1+randDie2+GameManager.instance.player1.spaceOn)%41].gameObject.GetComponent<MoveTokenToSpace>().TriggerMove();
+		GameManager.instance.player1.spaceOn = (randDie1+randDie2+GameManager.instance.player1.spaceOn)%41;
+	}
+}
